5bbf807e200fc41755c89602dbe4b2dc2dfaaa90
David Huette <davidandreshuette@gmail.com>
2024-12-22 02:05:55 -0500

    añadir reseñas bien

--- services/reseñas/postResena.ts
+++ services/reseñas/postResena.ts
@@ -1,21 +1,22 @@
 import { AxiosRequestConfig } from "axios";
 import Api from "../api";
 import * as SecureStore from "expo-secure-store";
+import { ubicationResponse } from "../../interfaces/ubication/UbicationResponse";
 import { ComentarioDTO, CreateComentarioDTO } from "../../interfaces/comentarios/ComentarioDto";
+import { CreateResenaDTO, ResenaDTO } from "../../interfaces/reseña/ResenaDto";
 
-export async function publicarComentario(comentario: CreateComentarioDTO) {
+export async function publicarResena(resena: CreateResenaDTO) {
     const api = await Api.getInstance();
     const token = await SecureStore.getItemAsync("session");
     const options: AxiosRequestConfig = {
-        url: "/comentarios",
+        url: "/resenas",
         headers: {
             Authorization: `Bearer ${token}`,
           },
     };
     try {
-        console.log(comentario);
-        const response = await api.post<CreateComentarioDTO,ComentarioDTO>(
-            comentario,
+        const response = await api.post<CreateResenaDTO,ResenaDTO>(
+            resena,
             options
         );
         return response.data;
